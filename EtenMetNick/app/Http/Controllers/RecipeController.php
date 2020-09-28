dbd9475f41156d2f62744c9ac25afdc92af0785c
eva <evavanderkroft@gmail.com>
2020-09-28 12:19:57 +0200

    Added Laravel Plugin + started first view & routes.
    Made sure that my laravel plugin worked and started to work with the first views and routes.

---
+++ EtenMetNick/app/Http/Controllers/RecipeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    public function show(){
+    return view('recipe');
+    }
+
+//    public function index(){
+//        return
+//    }
+
+
+}
--- EtenMetNick/resources/views/welcome.blade.php
+++ EtenMetNick/resources/views/welcome.blade.php
@@ -81,18 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Eten met Nick
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laracasts.com">inloggen</a>
+                    <a href="https://laravel-news.com">account aanmaken</a>
+                    <a href="{{route('recipe')}}">Recepten</a>
+
                 </div>
             </div>
         </div>
