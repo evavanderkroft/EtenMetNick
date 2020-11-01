729587e61da00acfc9ac4f26d0b3d06362dbb544
eva <evavanderkroft@gmail.com>
2020-11-01 21:44:54 +0100

    Cleanup files. More structure and more neat.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,6 @@
 <!DOCTYPE html>
 @extends('layouts.app')
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    </head>
     <body>
     @section('content')
     <div class="container">
@@ -38,4 +27,4 @@
     @section('footer')
         @endsection
     </body>
-</html>
+
